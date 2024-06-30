f96fe1bcf5eda820c5b33df790f56d7b23a2d087
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2024-06-30 16:15:33 +0100

    created public directory

--- src/index.ts
+++ src/index.ts
@@ -29,8 +29,8 @@ import customerRouter from "./routes/customerRoutes"
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
 }
-console.log(path.resolve(__dirname, "../front-end/dist"))
-app.use(express.static(path.resolve(__dirname, "../front-end/dist")))
+
+app.use(express.static(path.resolve(__dirname, "/public")))
 
 app.use(express.json())
 app.use(cookieParser())
@@ -45,7 +45,7 @@ app.use("/api/v1/store", authenticateUser, storeRouter)
 app.use("/api/v1/customer", authenticateUser, customerRouter)
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../front-end/dist", "index.html"))
+  res.sendFile(path.resolve(__dirname, "/public", "index.html"))
 })
 
 app.use("*", (req, res) => {
