f12842fabbeb68b9245bf1b3e0515e373cb6baa0
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2024-07-12 15:21:12 +0100

    .

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -7,17 +7,17 @@ const Navbar = () => {
   const { showSidebar, setShowSidebar } = useDashboardContext()
   return (
     <div
-      className={`flex justify-between items-center p-3 px-2 lg:px-5 h-[80px] md:h-[100px] bg-white w-full sticky top-0 z-10`}
+      className={`flex justify-between items-center p-3 px-2 lg:px-5 h-[80px] md:h-[100px] bg-[var(--bgColor)] w-full sticky top-0 z-10`}
     >
       <button
-        className='text-xl md:text-3xl text-indigo-500 hover:text-indigo-800 ease-in-out duration-300 lg:opacity-0'
+        className='text-xl md:text-3xl text-[var(--primary)] hover:text-[var(--hoverColor)] ease-in-out duration-300 lg:opacity-0'
         onClick={() => {
           setShowSidebar(!showSidebar)
         }}
       >
         <FaAlignLeft />
       </button>
-      <Logo container='w-[80px] md:w-[100px] block lg:hidden ' image='w-full' />
+      <Logo container='w-[60px] md:w-[100px] block lg:hidden ' image='w-full' />
       <h1 className='hidden lg:block text-3xl'>Dashboard</h1>
       <Logout />
     </div>
